54c488b3209df6331bef41d9c9de812cc377fbf0
SeoungminKwon <tjdalsrnjs10@gmail.com>
2023-11-14 10:23:45 +0900

    lec2

--- src/main/java/com/example/sb/HomeController.java
+++ src/main/java/com/example/sb/HomeController.java
@@ -1,2 +1,23 @@
-package com.example.sb;public class HomeController {
+package com.example.sb;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    @ResponseBody
+    String showMain(){
+        System.out.println("안녕하세여tetetete");
+        return "안녕하세요";
+    }
+
+    @GetMapping("/about")
+    @ResponseBody
+    String showAbout(){
+        return "어바웃";
+    }
 }
